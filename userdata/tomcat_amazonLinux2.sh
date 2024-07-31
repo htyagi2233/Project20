dfd0751e7c4c3317b0ff1d14a69d5b99d9f5f35b
htyagi2233 <164169058+htyagi2233@users.noreply.github.com>
2024-07-31 16:41:56 +0530

    Update tomcat_amazonLinux2.sh

--- userdata/tomcat_amazonLinux2.sh
+++ userdata/tomcat_amazonLinux2.sh
@@ -1,15 +1,12 @@
 # Update the package list
 sudo yum update -y
 
-# Upgrade all installed packages to their latest versions
-sudo yum upgrade -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
 # Install OpenJDK 11
 sudo yum install java-11-openjdk-devel -y
-
-# Install Tomcat 9 and its related packages
-# Note: Tomcat 9 might not be available in the default repositories, so we'll use Amazon's extras repository if available.
-sudo amazon-linux-extras install tomcat9 -y
 
 # Install Git
 sudo yum install git –y
